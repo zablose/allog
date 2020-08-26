cdd42029d396d9394a260a106851a213da996d3c
Sergejs Zablockis <zablose@gmail.com>
2020-08-26 12:59:53 +0100

    First test with DB access.

--- laravel/tests/Feature/ClientTest.php
+++ laravel/tests/Feature/ClientTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use Faker\Factory as Faker;
+use GuzzleHttp\Client;
 use Tests\TestCase;
 
 class ClientTest extends TestCase
@@ -10,5 +12,20 @@ class ClientTest extends TestCase
     public function is_accessible()
     {
         $this->get('/client')->assertOk()->assertSeeText('Allog Client');
+    }
+
+    /** @test */
+    public function sends_get_data()
+    {
+        $faker = Faker::create();
+
+        $uuid = $faker->uuid;
+
+        $guzzle = new Client();
+        $body = $guzzle->get(secure_url('/client?name=testing&uuid='.$uuid))->getBody();
+
+        $this->assertStringContainsString('Allog Client', $body);
+
+        $this->assertDatabaseHas('requests_allog_local', ['get' => '{"url":"client","uuid":"'.$uuid.'"}']);
     }
 }
