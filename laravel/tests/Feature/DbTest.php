7d2d20c460e8b7462b8d604fae24f9c4e615db12
Sergejs Zablockis <zablose@gmail.com>
2020-09-09 15:39:22 +0100

    Test Db::getLatestClients method.

--- laravel/tests/Feature/DbTest.php
+++ laravel/tests/Feature/DbTest.php
@@ -9,16 +9,38 @@ use Zablose\Allog\Db;
 
 class DbTest extends TestCase
 {
+    protected ?Db $db = null;
+
+    public function db(): Db
+    {
+        if ($this->db === null) {
+            $this->db = new Db((new Config())->read(__DIR__.'/../../../.env')->debugOn());
+        }
+
+        return $this->db;
+    }
+
     /** @test */
     public function adds_client()
     {
-        (new Db((new Config())->read(__DIR__.'/../../../.env')->debugOn()))
-            ->addClient($name = 'testing_client', 'token', '127.0.0.2');
+        $this->db()->addClient($name = 'testing_client', 'token', '127.0.0.2');
 
         $model = Clients::where(compact('name'))->first();
 
         $this->assertTrue($model !== null);
 
         $model->delete();
+    }
+
+    /** @test */
+    public function gets_latest_clients()
+    {
+        $this->db()->addClient($name = 'testing_client', 'token', '127.0.0.2');
+
+        $client = $this->db()->getLatestClients(1);
+
+        $this->assertTrue($client[0]->name === $name);
+
+        Clients::where(compact('name'))->first()->delete();
     }
 }
