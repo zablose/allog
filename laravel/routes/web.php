648238e77fc80c39632e4a988bf6a4789c48079e
Sergejs Zablockis <zablose@gmail.com>
2020-09-14 20:13:03 +0100

    Update to use Laravel 8.2 for testing.

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\AllogController;
 use Illuminate\Support\Facades\Route;
 
-Route::any('/server', 'AllogController@server');
-Route::any('/client', 'AllogController@client');
-Route::any('/server-with-remote-client', 'AllogController@server_with_remote_client');
+Route::any('/server', [AllogController::class, 'server']);
+Route::any('/client', [AllogController::class, 'client']);
+Route::any('/server-with-remote-client', [AllogController::class, 'server_with_remote_client']);
