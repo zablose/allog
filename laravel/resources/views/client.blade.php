893605065c48a173b0db25deb88337fb73f5dd5b
Sergejs Zablockis <zablose@gmail.com>
2025-04-11 13:48:31 +0100

    Add info page '/' with links of available routes.

--- laravel/resources/views/client.blade.php
+++ laravel/resources/views/client.blade.php
@@ -2,11 +2,10 @@
 <html lang="en">
 <head>
     <title>Allog Client</title>
-    <link rel="icon" href="data:,">
 </head>
 <body>
 <h1>Allog Client</h1>
 <h2>Debug</h2>
-<pre>{{ print_r($client->getError()) }}</pre>
+<pre>{!! print_r($client->getError(), true) !!}</pre>
 </body>
 </html>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,5 +3,6 @@
 use App\Http\Controllers\AllogController;
 use Illuminate\Support\Facades\Route;
 
+Route::view('/', 'info');
 Route::any('/client', [AllogController::class, 'client']);
 Route::any('/server', [AllogController::class, 'server']);
